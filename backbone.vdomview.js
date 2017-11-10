bebb2c83e625f496a15cb53407b5333af0d427b6
JC Brand <jc@opkode.com>
2017-11-10 20:38:06 +0100

    Initial files

---
+++ backbone.vdomview.js
@@ -0,0 +1,39 @@
+/*!
+ * Backbone.VDOMView
+ *
+ * Copyright (c) 2017, JC Brand <jc@opkode.com>
+ */
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define([
+            "virtual-dom",
+            "vdom-parser",
+            "underscore",
+            "backbone"
+        ], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // CommonJS-like environments
+        module.exports = factory(
+            require('virtual-dom'),
+            require('vdom-parser'),
+            require('underscore'),
+            require('backbone'),
+        );
+   }
+}(this, function (vdom, vdom_parser, _, Backbone) {
+    "use strict";
+
+    Backbone.VDOMView = Backbone.View.extend({
+
+        render () {
+            const patches = vdom.diff(
+                vdom_parser(this.el),
+                vdom_parser(this.renderHTML())
+            );
+            const root = vdom.patch(this.el, patches);
+            this.setElement(root);
+            return this;
+        }
+    });
+    return Backbone.VDOMView;
+}));
