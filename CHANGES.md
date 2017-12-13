daec3e96a08ccf341fef84c2d913cbd40924e79d
JC Brand <jc@opkode.com>
2017-12-13 20:08:32 +0000

    Use snabbdom as the virtual dom implementation

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.0.3 (2017-12-13)
+
+- Use Snabbdom, instead of virtual-dom and vdom-parser, for the virtual-DOM implementation
+
 ## 0.0.2 (2017-11-10)
 
 - Add `beforeRender` and `afterRender` lifecycle methods.
--- backbone.vdomview.js
+++ backbone.vdomview.js
@@ -6,22 +6,92 @@
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         define([
-            "virtual-dom",
-            "vdom-parser",
+            "snabbdom",
+            "snabbdom-attributes",
+            "snabbdom-class",
+            "snabbdom-dataset",
+            "snabbdom-eventlisteners",
+            "snabbdom-props",
+            "snabbdom-style",
+            "tovnode",
             "underscore",
             "backbone"
         ], factory);
     } else if (typeof module === 'object' && module.exports) {
         // CommonJS-like environments
         module.exports = factory(
-            require('virtual-dom'),
-            require('vdom-parser'),
+            require('snabbdom'),
+            require('snabbdom-attributes'),
+            require('snabbdom-class'),
+            require('snabbdom-dataset'),
+            require('snabbdom-eventlisteners'),
+            require('snabbdom-props'),
+            require('snabbdom-style'),
+            require('tovnode'),
             require('underscore'),
             require('backbone')
         );
    }
-}(this, function (vdom, vdom_parser, _, Backbone) {
+}(this, function (
+        snabbdom,
+        snabbdom_attributes,
+        snabbdom_class,
+        snabbdom_dataset,
+        snabbdom_eventlisteners,
+        snabbdom_props,
+        snabbdom_style,
+        tovnode,
+        _,
+        Backbone) {
     "use strict";
+
+    let domParser = new DOMParser();
+    const patch = snabbdom.init([
+        snabbdom_attributes.default,
+        snabbdom_class.default,
+        snabbdom_dataset.default,
+        snabbdom_eventlisteners.default,
+        snabbdom_props.default,
+        snabbdom_style.default
+    ]);
+
+    function parseHTMLToDOM (html_str) {
+        /* Parses a string with HTML and returns a DOM element.
+         *
+         * Forked from vdom_parser:
+         *      https://github.com/bitinn/vdom-parser
+         */
+        if (typeof html_str !== 'string') {
+            throw new Error('Invalid parameter type in parseHTMLToDOM');
+        }
+        if ( !('DOMParser' in window) ) {
+            throw new Error(
+                'DOMParser is not available, '+
+                'so parsing string to DOM node is not possible.');
+        }
+        if (!html_str) {
+            return document.createTextNode('');
+        }
+        domParser = domParser || new DOMParser();
+        const doc = domParser.parseFromString(html_str, 'text/html');
+
+        // most tags default to body
+        if (doc.body.firstChild) {
+            return doc.getElementsByTagName('body')[0].firstChild;
+
+        // some tags, like script and style, default to head
+        } else if (doc.head.firstChild && (doc.head.firstChild.tagName !== 'TITLE' || doc.title)) {
+            return doc.head.firstChild;
+
+        // special case for html comment, cdata, doctype
+        } else if (doc.firstChild && doc.firstChild.tagName !== 'HTML') {
+            return doc.firstChild;
+
+        // other element, such as whitespace, or html/body/head tag, fallback to empty text node
+        } else {
+            return document.createTextNode('');
+        }
+    }
 
     Backbone.VDOMView = Backbone.View.extend({
 
@@ -29,12 +99,10 @@
             if (_.isFunction(this.beforeRender)) {
                 this.beforeRender();
             }
-            const patches = vdom.diff(
-                vdom_parser(this.el),
-                vdom_parser(this.renderHTML())
-            );
-            const root = vdom.patch(this.el, patches);
-            this.setElement(root);
+            const new_vnode = tovnode.toVNode(parseHTMLToDOM(this.renderHTML()));
+            const root = patch(this.vnode || this.el, new_vnode);
+            this.setElement(root.elm);
+            this.vode = root;
             if (_.isFunction(this.afterRender)) {
                 this.afterRender();
             }
